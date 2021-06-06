1a7fa345fbd8ad0063827ad4ab181bf296225135
Posey <adeposi60@gmail.com>
2021-06-06 09:35:24 +0100

    adding dist

---
+++ dist/precache-manifest.1a8a6f91de4fff9779df8595baee3990.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3f115125b6df16744a",
+    "url": "/pwa-coursework/css/app.18dd0f9a.css"
+  },
+  {
+    "revision": "c50022697cd94ab2eb4d16c3a9322a73",
+    "url": "/pwa-coursework/img/agric.c5002269.png"
+  },
+  {
+    "revision": "84e2472c51154df5ede8901e731629a3",
+    "url": "/pwa-coursework/img/art.84e2472c.png"
+  },
+  {
+    "revision": "a47e003fa61e222a4dd33bdeac23c9ce",
+    "url": "/pwa-coursework/img/biology.a47e003f.png"
+  },
+  {
+    "revision": "6545d0bc83529f752bc754cb2f22d9cd",
+    "url": "/pwa-coursework/img/chemistry.6545d0bc.png"
+  },
+  {
+    "revision": "8171544b5b55e895b5f033b9e189931e",
+    "url": "/pwa-coursework/img/computers.8171544b.png"
+  },
+  {
+    "revision": "4a759a83c244a9d91a32dea7339d631b",
+    "url": "/pwa-coursework/img/economics.4a759a83.png"
+  },
+  {
+    "revision": "056c90887df4be13db7236c5b7b9ab4e",
+    "url": "/pwa-coursework/img/english.056c9088.png"
+  },
+  {
+    "revision": "92cfdf871845ef859712c39aad12cf16",
+    "url": "/pwa-coursework/img/history.92cfdf87.png"
+  },
+  {
+    "revision": "4598fc19f84574ad03462fd5d8347760",
+    "url": "/pwa-coursework/img/maths.4598fc19.png"
+  },
+  {
+    "revision": "25994a349e78d92a83a77e999327c3d4",
+    "url": "/pwa-coursework/img/nutrition.25994a34.png"
+  },
+  {
+    "revision": "3067a16aa464794c2e028574ef39d2a4",
+    "url": "/pwa-coursework/img/physics.3067a16a.png"
+  },
+  {
+    "revision": "040c66c4c4086558c0a146ad142f8df6",
+    "url": "/pwa-coursework/img/sports.040c66c4.png"
+  },
+  {
+    "revision": "9d525f249907b2d5f88a90fe44072558",
+    "url": "/pwa-coursework/index.html"
+  },
+  {
+    "revision": "ac3f115125b6df16744a",
+    "url": "/pwa-coursework/js/app.f009f0a8.js"
+  },
+  {
+    "revision": "6722db6645252ccf4031",
+    "url": "/pwa-coursework/js/chunk-vendors.aefb9fb4.js"
+  },
+  {
+    "revision": "690bd79504ba5f1eb726ff1a5545ab05",
+    "url": "/pwa-coursework/lessons.webmanifest"
+  },
+  {
+    "revision": "bd5554855aca0380660487b55d9af689",
+    "url": "/pwa-coursework/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-coursework/robots.txt"
+  }
+]);
