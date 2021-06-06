91e20ed9c636fc44a060b3d9d3b8d6f701389da1
Posey <adeposi60@gmail.com>
2021-06-06 06:15:22 +0100

    adding dist subtree

---
+++ precache-manifest.ed334d9b221027cc75eda18bc7b45731.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d08a923d7efc563cef5",
+    "url": "/pwa-coursework/css/app.18dd0f9a.css"
+  },
+  {
+    "revision": "46b25e123a52d3ce0f5b11d6ffd2cf02",
+    "url": "/pwa-coursework/index.html"
+  },
+  {
+    "revision": "8d08a923d7efc563cef5",
+    "url": "/pwa-coursework/js/app.b94cd57d.js"
+  },
+  {
+    "revision": "757e9c63e833e10e6884",
+    "url": "/pwa-coursework/js/chunk-vendors.d0dc9178.js"
+  },
+  {
+    "revision": "690bd79504ba5f1eb726ff1a5545ab05",
+    "url": "/pwa-coursework/lessons.webmanifest"
+  },
+  {
+    "revision": "a04ec2444cb198435296830d1339247c",
+    "url": "/pwa-coursework/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-coursework/robots.txt"
+  },
+  {
+    "revision": "2a7d0c3cad4f6f75781b31c535af849f",
+    "url": "/pwa-coursework/service-worker.js"
+  }
+]);
